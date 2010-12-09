43593e7340a6c2ba663fd8c568ab33f5069d4588
vajda <vajda@13f79535-47bb-0310-9956-ffa450edef68>
2010-12-09 21:44:46 +0000

    - fixed bug with faulty Java version logic causing crashes with Java 1.5
    git-svn-id: https://svn.apache.org/repos/asf/lucene/pylucene/trunk@1044137 13f79535-47bb-0310-9956-ffa450edef68

--- jcc/jcc/sources/JCCEnv.cpp
+++ jcc/jcc/sources/JCCEnv.cpp
@@ -110,20 +110,25 @@ void JCCEnv::set_vm(JavaVM *vm, JNIEnv *vm_env)
         vm_env->GetMethodID(_obj, "getClass",
                             "()Ljava/lang/Class;");
 
-    if (vm_env->GetVersion() >= 0x00010005)
+
+    jclass iterable = vm_env->FindClass("java/lang/Iterable");
+
+    if (iterable == NULL) /* JDK < 1.5 */
+    {
+        vm_env->ExceptionClear();
+        _mids[mid_iterator] = NULL;
+        _mids[mid_iterator_next] = NULL;
+    }
+    else
     {
         _mids[mid_iterator] =
-            vm_env->GetMethodID(vm_env->FindClass("java/lang/Iterable"),
+            vm_env->GetMethodID(iterable,
                                 "iterator", "()Ljava/util/Iterator;");
         _mids[mid_iterator_next] =
             vm_env->GetMethodID(vm_env->FindClass("java/util/Iterator"),
                                 "next", "()Ljava/lang/Object;");
     }
-    else
-    {
-        _mids[mid_iterator] = NULL;
-        _mids[mid_iterator_next] = NULL;
-    }
+
 
     _mids[mid_enumeration_nextElement] =
         vm_env->GetMethodID(vm_env->FindClass("java/util/Enumeration"),
