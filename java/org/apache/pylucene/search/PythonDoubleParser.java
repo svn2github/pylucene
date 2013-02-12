a42040a59de688b020e084f821daab5972c2e92b
vajda <vajda@13f79535-47bb-0310-9956-ffa450edef68>
2013-02-12 08:09:14 +0000

    added missing termsEnum() method
    git-svn-id: https://svn.apache.org/repos/asf/lucene/pylucene/trunk@1445048 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/pylucene/search/PythonDoubleParser.java
+++ java/org/apache/pylucene/search/PythonDoubleParser.java
@@ -15,6 +15,10 @@
 
 package org.apache.pylucene.search;
 
+import java.io.IOException;
+
+import org.apache.lucene.index.Terms;
+import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.search.FieldCache;
 import org.apache.lucene.util.BytesRef;
 
@@ -47,4 +51,6 @@ public class PythonDoubleParser implements FieldCache.DoubleParser {
 
     public native void pythonDecRef();
     public native double parseDouble(BytesRef ref);
+    public native TermsEnum termsEnum(Terms terms)
+        throws IOException;
 }
