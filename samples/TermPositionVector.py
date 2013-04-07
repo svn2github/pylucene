7eb06af6a4a12a3d9cf79231781cd552bd340ad4
vajda <vajda@13f79535-47bb-0310-9956-ffa450edef68>
2013-04-07 20:58:14 +0000

    migrated TermPositionVector.py
    git-svn-id: https://svn.apache.org/repos/asf/lucene/pylucene/trunk@1465461 13f79535-47bb-0310-9956-ffa450edef68

--- samples/TermPositionVector.py
+++ samples/TermPositionVector.py
@@ -1,37 +1,53 @@
-from lucene import \
-    StandardAnalyzer, RAMDirectory, Document, Field, Version, \
-    IndexWriter, IndexReader, TermPositionVector, initVM
+
+import lucene
+
+from org.apache.lucene.analysis.miscellaneous import LimitTokenCountAnalyzer
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.store import RAMDirectory
+from org.apache.lucene.document import Document, Field, FieldType
+from org.apache.lucene.util import BytesRef, BytesRefIterator, Version
+from org.apache.lucene.index import \
+    IndexWriterConfig, IndexWriter, DirectoryReader
 
 if __name__ == '__main__':
-    initVM()
+    lucene.initVM()
 
 directory = RAMDirectory()
-iwriter = IndexWriter(directory, StandardAnalyzer(Version.LUCENE_CURRENT),
-                      True, IndexWriter.MaxFieldLength.LIMITED)
+iconfig = IndexWriterConfig(Version.LUCENE_CURRENT, LimitTokenCountAnalyzer(StandardAnalyzer(Version.LUCENE_CURRENT), 100))
+iwriter = IndexWriter(directory, iconfig)
+
+ft = FieldType()
+ft.setIndexed(True)
+ft.setStored(True)
+ft.setTokenized(True)
+ft.setStoreTermVectors(True)
+ft.setStoreTermVectorOffsets(True)
+ft.setStoreTermVectorPositions(True)
+
 ts = ["this bernhard is the text to be index text",
-      "this claudia is the text to be index"]
+      "this claudia is the text to be indexed"]
 for t in ts:
     doc = Document()
-    doc.add(Field("fieldname", t,
-                  Field.Store.YES, Field.Index.ANALYZED,
-                  Field.TermVector.WITH_POSITIONS_OFFSETS))
+    doc.add(Field("fieldname", t, ft))
     iwriter.addDocument(doc)
+
 iwriter.commit()
 iwriter.close()
+ireader = DirectoryReader.open(directory)
 
-ireader = IndexReader.open(directory, True)
-tpv = TermPositionVector.cast_(ireader.getTermFreqVector(0, 'fieldname'))
+for doc in xrange(0, len(ts)):
+    tv = ireader.getTermVector(doc, "fieldname")
+    termsEnum = tv.iterator(None)
 
-for (t,f,i) in zip(tpv.getTerms(),tpv.getTermFrequencies(),xrange(100000)):
-    print 'term %s' % t
-    print '  freq: %i' % f
-    try:
-        print '  pos: ' + str([p for p in tpv.getTermPositions(i)])
-    except:
-        print '  no pos'
-    try:
-        print '  off: ' + \
-              str(["%i-%i" % (o.getStartOffset(), o.getEndOffset())
-                   for o in tpv.getOffsets(i)])
-    except:
-        print '  no offsets'
+    for term in BytesRefIterator.cast_(termsEnum):
+        dpEnum = termsEnum.docsAndPositions(None, None)
+        dpEnum.nextDoc()  # prime the enum which works only for the current doc
+        freq = dpEnum.freq()
+
+        print 'term:', term.utf8ToString()
+        print '  freq:', freq
+
+        for i in xrange(freq):
+            print "  pos:", dpEnum.nextPosition()
+            print "  off: %i-%i" %(dpEnum.startOffset(), dpEnum.endOffset())
+    print
