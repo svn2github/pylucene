9a20da7bf23b49acb47d063385f746cbf359f15c
vajda <vajda@13f79535-47bb-0310-9956-ffa450edef68>
2016-09-09 09:21:59 +0000

    got SearchFiles.py to run
    git-svn-id: https://svn.apache.org/repos/asf/lucene/pylucene/trunk@1759964 13f79535-47bb-0310-9956-ffa450edef68

--- samples/SearchFiles.py
+++ samples/SearchFiles.py
@@ -4,16 +4,15 @@ INDEX_DIR = "IndexFiles.index"
 
 import sys, os, lucene
 
-from java.io import File
+from java.nio.file import Paths
 from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.index import DirectoryReader
 from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.store import SimpleFSDirectory
 from org.apache.lucene.search import IndexSearcher
-from org.apache.lucene.util import Version
 
 """
-This script is loosely based on the Lucene (java implementation) demo class 
+This script is loosely based on the Lucene (java implementation) demo class
 org.apache.lucene.demo.SearchFiles.  It will prompt for a search query, then it
 will search the Lucene index in the current directory called 'index' for the
 search query entered against the 'contents' field.  It will then display the
@@ -31,8 +30,7 @@ def run(searcher, analyzer):
 
         print
         print "Searching for:", command
-        query = QueryParser(Version.LUCENE_CURRENT, "contents",
-                            analyzer).parse(command)
+        query = QueryParser("contents", analyzer).parse(command)
         scoreDocs = searcher.search(query, 50).scoreDocs
         print "%s total matching documents." % len(scoreDocs)
 
@@ -45,8 +43,8 @@ if __name__ == '__main__':
     lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     print 'lucene', lucene.VERSION
     base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-    directory = SimpleFSDirectory(File(os.path.join(base_dir, INDEX_DIR)))
+    directory = SimpleFSDirectory(Paths.get(os.path.join(base_dir, INDEX_DIR)))
     searcher = IndexSearcher(DirectoryReader.open(directory))
-    analyzer = StandardAnalyzer(Version.LUCENE_CURRENT)
+    analyzer = StandardAnalyzer()
     run(searcher, analyzer)
     del searcher
